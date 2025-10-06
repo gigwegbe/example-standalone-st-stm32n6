7cf08cd2900e06d32116e6c2fc19bdb0610543a1
francovaro <francovaro@gmail.com>
2025-10-06 15:46:23 +0200

    missing fuse programming

--- flash.sh
+++ flash.sh
@@ -2,7 +2,8 @@
 set -e
 
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
-EL="$(dirname "$(which STM32_Programmer_CLI)")/ExternalLoader/MX66UW1G45G_STM32N6570-DK.stldr"
+PATH="/Applications/STMicroelectronics/STM32Cube/STM32CubeProgrammer/STM32CubeProgrammer.app/Contents/MacOs/bin:$PATH"
+EL="$(dirname "$(which STM32_Programmer_CLI)")/ExternalLoader/MX25UM51245G_STM32N6570-NUCLEO.stldr"
 
 TARGET=$1
 
@@ -16,6 +17,7 @@ arm-none-eabi-objcopy -O ihex build/Project.elf build/Project.hex
 arm-none-eabi-objcopy -O binary build/Project.elf build/Project.bin
 
 # sign bin
+rm -f build/project-trusted.bin
 STM32_SigningTool_CLI -s -bin build/Project.bin -nk -t fsbl -hv 2.1 -o build/project-trusted.bin
 
 echo "Flashing ${TARGET}"
@@ -27,7 +29,7 @@ elif [ "$TARGET" == "all" ]; then
 elif [ "$TARGET" == "weights" ]; then
     STM32_Programmer_CLI -c port=SWD mode=HOTPLUG ap=1 -el ${EL} -hardRst -w Model/network_data.hex
 elif [ "$TARGET" == "bootloader" ]; then
-    STM32_Programmer_CLI -c port=SWD mode=HOTPLUG ap=1 -el ${EL} -hardRst -w ai_fsbl.hex
+    STM32_Programmer_CLI -c port=SWD mode=HOTPLUG ap=1 -el ${EL} -hardRst -w ai_fsbl_cut_2_0.hex
 else
     echo "Invalid target: $TARGET"
     exit 1
